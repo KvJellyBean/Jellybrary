855b062d7539f8851de712582eb0c4154b937abe
KvJellyBean <kevinnatan91@gmail.com>
2023-12-24 20:56:18 +0700

    Make add book functionality to the website
    Create add book action, book object and insert it into an array, render book function,, and remove book

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,67 @@
+const myLibrary = [];
 const addButton = document.querySelector('#library-container .addButton');
+const bookContainer = document.querySelector('.book-list');
 const dialog = document.querySelector('#addBook');
+const form = document.querySelector('#addBook form');
 
+// Event handler for showing diialog button
 addButton.addEventListener('click', (e) => {
     dialog.showModal();
 });
+
+// Add new book
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    console.log('Book inserted');
+});
+
+// Book constructor
+function Book(title, author, pages, url, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.url = url;
+    this.status = status;
+}
+
+// Function to create book object and render it
+function addBookToLibrary() {
+    const bookTitle = document.querySelector('#bookName').value;
+    const bookAuthor = document.querySelector('#bookAuthor').value;
+    const bookPages = document.querySelector('#pages').value;
+    const bookCover = document.querySelector('#cover').value;
+    const bookStatus = document.querySelector('#status').checked;
+    const books = new Book(bookTitle, bookAuthor, bookPages, bookCover, bookStatus);
+    myLibrary.push(books);
+    renderBooks();
+}
+
+// Function for rendering the books into the website
+function renderBooks() {
+    bookContainer.innerHTML = '';
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('book');
+        card.innerHTML = `
+            <img src="${myLibrary[i].url}" alt="${myLibrary[i].title} Cover">
+                <div class="description">
+                <h3>${myLibrary[i].title}</h3>
+                <p>${myLibrary[i].author}</p>
+                <p>${myLibrary[i].pages}</p>
+                <div class="buttons">
+                    <button type="button" class="${myLibrary[i].status ? 'complete' : 'not-complete'}">${myLibrary[i].status ? 'Completed' : 'Not Completed'}</button>
+                    <span class="material-symbols-outlined" onClick="removeBook(${i})"> delete </span>
+                </div>
+            </div>
+        `;
+        bookContainer.appendChild(card);
+    }
+}
+
+// Function to remove a book in the library
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    renderBooks();
+}
